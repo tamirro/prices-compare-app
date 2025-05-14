e881e16d20ea21664fbb377176581edad52f69a0
Tamir Rozenberg <tamir.rozenberg@mail.huji.ac.il>
2025-05-14 08:39:06 +0300

    Update app.py with ngrok URL

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown("""
 
 @st.cache_data(ttl=3600)
 def load_data(table_name):
-    response = requests.get(f"http://10.58.79.2:5000/data/{table_name}")
+    response = requests.get(f" https://edb3-2a06-c701-bc8e-9800-e882-70fd-a82f-ef9e.ngrok-free.app/data/{table_name}")
     return pd.DataFrame(response.json())
 
 your_store = load_data('your_store')
